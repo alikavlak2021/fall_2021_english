077408b0b4c140f4fee4ace396548778932f53ab
apple <apple@apples-imac-6.attlocal.net>
2021-12-04 13:10:38 -0500

    Committed

--- src/lambda_functional_programming/Fp03.java
+++ src/lambda_functional_programming/Fp03.java
@@ -3,9 +3,10 @@ package lambda_functional_programming;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Fp03 {
-	
+
 	public static void main(String[] args) {
 		
 		List<String> l = new ArrayList<>();
@@ -29,11 +30,16 @@ public class Fp03 {
         System.out.println();
         printDistinctSortedByLastChar(l);
         System.out.println();
-        printSortedByLengthFirstThenSortedByInitial(l); 
+        printSortedByLengthThenSortedByInitial(l);
         System.out.println();
-        removeIfTheLengthIsGreaterThanFive(l);
-        
-		
+        //removeIfTheLengthIsGreaterThanFive(l);
+        //printRemoveIfStartingWithAEndingWithN01(l);
+        //printRemoveIfStartingWithAEndingWithN02(l);
+        //removeIfLengthIsBetween8And10EndingWithO(l);
+        System.out.println(checkLengthToBeLessThanTwelve(l));
+        System.out.println(checkNotToStartWithX(l));
+        System.out.println(checkAnyOneEndsWithR(l));
+
 	}
 	
 	//1) Create a method to print all list elements in uppercase
@@ -43,16 +49,14 @@ public class Fp03 {
 	}
 	
 	//2.Way:
-//	public static void printInUpperCases02(List<String> list) {			
-//		list.replaceAll(String::toUpperCase);		
+//	public static void printInUpperCases02(List<String> list) {				
+//		list.replaceAll(String::toUpperCase);			
 //		System.out.println(list);
 //	}
 	
 	//2) Create a method to print the elements after ordering according to their lengths
-	public static void printElementsSortedByLength(List<String> list) {
-		
-		list.stream().sorted(Comparator.comparing(String::length)).forEach(Utils::printInTheSameLineWithSpace);
-		
+	public static void printElementsSortedByLength(List<String> list) {		
+		list.stream().sorted(Comparator.comparing(String::length)).forEach(Utils::printInTheSameLineWithSpace);		
 	}
 	
 	//3) Create a method to print the elements after ordering according to their lengths (In reverse order)
@@ -66,20 +70,52 @@ public class Fp03 {
 	}
 	
 	//5) Create a method to sort the elements according to their lengths then according to their first character
-	public static void printSortedByLengthFirstThenSortedByInitial(List<String> list) {		
+	public static void printSortedByLengthThenSortedByInitial(List<String> list) {		
 		list.
 			stream().
-			sorted(Comparator.comparing(String::length).thenComparing(Utils::getFirstChar)).
-			forEach(Utils::printInTheSameLineWithSpace);				
+			sorted(Comparator.comparing(String::length).thenComparing(t->t.charAt(0))).
+			forEach(Utils::printInTheSameLineWithSpace);			
 	}
-
+	
 	//6) Remove the elements if the length of the element is greater than 5
-	public static void removeIfTheLengthIsGreaterThanFive(List<String> list) {
-		
-		list.removeIf(t->t.length()>5);
-		
-		System.out.println(list);
-		
+//	public static void removeIfTheLengthIsGreaterThanFive(List<String> list) {		
+//		list.removeIf(t->t.length()>5);		
+//		System.out.println(list);//Ali Ali Mark		
+//	}
+	
+	//7) Remove the elements if the element is starting with ‘A’, ‘a’ or ending with ‘N’, ‘n’	
+	//1.Way:
+//	public static void printRemoveIfStartingWithAEndingWithN01(List<String> list) {			
+//		Predicate<String> checkCondition = t -> t.charAt(0)=='A'||t.charAt(0)=='a'||t.charAt(t.length()-1)=='N'||t.charAt(t.length()-1)=='n';				
+//		list.removeIf(t-> checkCondition.test(t));		
+//		System.out.println(list);		
+//	}	
+	//2.Way:
+//	public static void printRemoveIfStartingWithAEndingWithN02(List<String> list) {	
+//		Predicate<String> checkCondition = t -> t.startsWith("A")||t.startsWith("a")||t.endsWith("N")||t.endsWith("n");
+//		list.removeIf(t->checkCondition.test(t));		
+//		System.out.println(list);	
+//	}
+	
+	//8) Remove the elements if the length is between 8 and 10 or ending with 'o'
+//	public static void removeIfLengthIsBetween8And10EndingWithO(List<String> list) {		
+//		Predicate<String> checkCondition = t -> (t.length()>7 && t.length()<11) || t.endsWith("o");		
+//		list.removeIf(t->checkCondition.test(t));		
+//		System.out.println(list);		
+//	}
+	
+	//9) Create a method to check if the lengths of all elements are less than 12
+	public static boolean checkLengthToBeLessThanTwelve(List<String> list) {		
+		return list.stream().allMatch(t->t.length()<12);		
 	}
-
+	
+	//10) Create a method to check if the initial of any element is not ‘X’
+	public static boolean checkNotToStartWithX(List<String> list) {		
+		return list.stream().noneMatch(t->t.startsWith("X"));		
+	}
+	
+	//11) Create a method to check if at least one of the elements ending with “r”
+	public static boolean checkAnyOneEndsWithR(List<String> list) {		
+		return list.stream().anyMatch(t->t.endsWith("r"));		
+	}
 }
